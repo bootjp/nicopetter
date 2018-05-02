0204d61cebfc084b359bf498f5ce49eae0f2d5cf
bootjp <oh@bootjp.me>
2018-05-02 17:35:19 +0900

    add command parse .

--- main/main.go
+++ main/main.go
@@ -8,24 +8,32 @@ import (
 
 	"net/url"
 
+	"fmt"
+
+	"log"
+
 	"github.com/ChimeraCoder/anaconda"
+	"github.com/bootjp/go_twitter_bot_for_nicopedia/domain/bot"
 	"github.com/bootjp/go_twitter_bot_for_nicopedia/domain/nicopedia"
 	"github.com/bootjp/go_twitter_bot_for_nicopedia/domain/twitter"
 	"github.com/bootjp/go_twitter_bot_for_nicopedia/item"
 	"github.com/bootjp/go_twitter_bot_for_nicopedia/store"
 	"github.com/mmcdole/gofeed"
+	"gopkg.in/urfave/cli.v2"
 )
 
+// Twitter base struct.
 type Twitter struct {
 	twitter.Authorization
 }
 
+// SendSNS is testable interface.
 type SendSNS interface {
 	Twitter(i *gofeed.Item, authorization *twitter.Authorization) error
 }
 
-// Twitter is Item to Twitter post.
-func (t *Twitter) PostTwitter(i *gofeed.Item) error {
+// PostTwitter is Item to Twitter post.
+func (t *Twitter) PostTwitter(i *gofeed.Item, mode *bot.Behavior) error {
 	api := anaconda.NewTwitterApiWithCredentials(
 		t.AccessToken,
 		t.AccessTokenSecret,
@@ -41,6 +49,7 @@ func (t *Twitter) PostTwitter(i *gofeed.Item) error {
 	}
 	ar := nicopedia.ParseArticleType(u)
 
+	fmt.Printf(mode.TweetFormat)
 	_, err = api.PostTweet(i.Title+ar.PostArticleExpression+" に "+i.Description+"というお絵カキコが投稿されたよ。"+i.Link, v)
 	if err != nil {
 		println(i.Title + ar.PostArticleExpression + " に " + i.Description + "というお絵カキコが投稿されたよ。" + i.Link)
@@ -50,8 +59,8 @@ func (t *Twitter) PostTwitter(i *gofeed.Item) error {
 	return nil
 }
 
-func routine() error {
-
+func routine(mode *bot.Behavior) error {
+	fmt.Printf("%v\n", mode)
 	f, err := item.Fetch("https://dic.nicovideo.jp/feed/rss/n/oekaki")
 	if err != nil {
 		return err
@@ -89,12 +98,14 @@ func routine() error {
 	sns := Twitter{au}
 
 	for _, v := range f {
-		err = sns.PostTwitter(v)
-		if err != nil {
+		if err = sns.PostTwitter(v, mode); err != nil {
 			println(v)
 			return err
 		}
-		r.SetLastUpdateTime(*v.PublishedParsed)
+
+		if err = r.SetLastUpdateTime(*v.PublishedParsed); err != nil {
+			return nil
+		}
 	}
 
 	if err != nil {
@@ -110,8 +121,23 @@ func routine() error {
 }
 
 func main() {
-	err := routine()
-	if err != nil {
-		panic(err)
+	app := cli.App{}
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:  "mode, m",
+			Value: "test",
+			Usage: "bot behavior mode.",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		mode, err := bot.NewBehavior(c.String("mode"))
+		if err != nil {
+			return err
+		}
+		return routine(mode)
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
 	}
 }
