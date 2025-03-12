5da06652a7501e5ab65aa9d2670721f725f3cb8d
Yoshiaki Ueda (bootjp) <contact@bootjp.me>
2025-03-13 00:54:26 +0900

    support movie article

--- domain/bot/behavior.go
+++ domain/bot/behavior.go
@@ -22,45 +22,54 @@ type Behavior struct {
 var (
 	// Gunyapetter is Nicopedia oekakiko tweet account. https://twitter.com/gunyapetter
 	Gunyapetter = &Behavior{
-		"%s%s に %s というお絵カキコが投稿されたよ。%s",
-		"https://dic.nicovideo.jp/feed/rss/n/oekaki",
-		false,
-		false,
-		"gunyapetter:",
+		TweetFormat: "%s%s に %s というお絵カキコが投稿されたよ。%s",
+		FeedURL:     "https://dic.nicovideo.jp/feed/rss/n/oekaki",
+		StorePrefix: "gunyapetter:",
 	}
 
 	// DulltterTmp is Nicopedia pikokakiko tweet account. https://twitter.com/dulltter_tmp
 	DulltterTmp = &Behavior{
-		"%s%s に %s というピコカキコが投稿されたよ。%s",
-		"https://dic.nicovideo.jp/feed/rss/n/mml",
-		false,
-		false,
-		"dulltter:",
+		TweetFormat: "%s%s に %s というピコカキコが投稿されたよ。%s",
+		FeedURL:     "https://dic.nicovideo.jp/feed/rss/n/mml",
+		StorePrefix: "dulltter:",
 	}
 
 	// NicopetterNewArticle is Nicopedia new general article tweet account.
 	NicopetterNewArticle = &Behavior{
-		"%s の記事ができたよ。%s",
-		"https://dic.nicovideo.jp/feed/rss/n/a",
-		true,
-		false,
-		"nicopetter_new:",
+		TweetFormat:         "%s の記事ができたよ。%s",
+		FeedURL:             "https://dic.nicovideo.jp/feed/rss/n/a",
+		EnableRedirectTitle: true,
+		StorePrefix:         "nicopetter_new:",
 	}
 
 	// NicopetterNewRedirectArticle is Nicopedia general article is to redirect tweet account.
 	NicopetterNewRedirectArticle = &Behavior{
-		"%s から %s へのリダイレクトができたよ。 %s",
-		"https://dic.nicovideo.jp/feed/rss/n/a",
-		true,
-		true,
-		"nicopetter_new_redirect:",
+		TweetFormat:         "%s から %s へのリダイレクトができたよ。 %s",
+		FeedURL:             "https://dic.nicovideo.jp/feed/rss/n/a",
+		EnableRedirectTitle: true,
+		FollowRedirect:      true,
+		StorePrefix:         "nicopetter_new_redirect:",
 	}
 	NicopetterModifyRedirectArticle = &Behavior{
-		"%s から %s へのリダイレクトができたよ。 %s",
-		"https://dic.nicovideo.jp/feed/rss/u/a",
-		true,
-		true,
-		"nicopetter_new_redirect:",
+		TweetFormat:         "%s から %s へのリダイレクトができたよ。 %s",
+		FeedURL:             "https://dic.nicovideo.jp/feed/rss/u/a",
+		EnableRedirectTitle: true,
+		FollowRedirect:      true,
+		StorePrefix:         "nicopetter_new_redirect:",
+	}
+	//NicopetterModifyMovieArticle = &Behavior{
+	//	TweetFormat:         "%s の
+	//	FeedURL:             "https://dic.nicovideo.jp/feed/rss/u/v",
+	//	EnableRedirectTitle: true,
+	//	FollowRedirect:      true,
+	//	StorePrefix:         "nicopetter_movie_update:",
+	//}
+	NicopetterNewMovieArticle = &Behavior{
+		TweetFormat:         "%s の動画記事ができたよ。%s",
+		FeedURL:             "https://dic.nicovideo.jp/feed/rss/n/v",
+		EnableRedirectTitle: true,
+		FollowRedirect:      false,
+		StorePrefix:         "nicopetter_movie_new:",
 	}
 )
 
@@ -77,6 +86,8 @@ func NewBehavior(mode string) (*Behavior, error) {
 		return NicopetterNewRedirectArticle, nil
 	case "nicopetter_modify_redirect":
 		return NicopetterModifyRedirectArticle, nil
+	case "nicopetter_new_movie":
+		return NicopetterNewMovieArticle, nil
 	default:
 		return nil, errors.New("mode is invalid string")
 	}
